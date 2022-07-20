76b47dc3d75e3b24f6ec78cf5f2579e0b13dc8f9
yuna1412 <85538944+yuna1412@users.noreply.github.com>
2022-07-20 11:51:12 +0900

    変更

--- population-bar-plot.js
+++ population-bar-plot.js
@@ -8,11 +8,14 @@ const PopulationBarPlot = {
     return {
       populations: [],  // 人口の配列 (State)
       maxPopulation: 1, // 人口の最大値 (State)
+      year: '',
+      value: '',
     };
   },
 
   /* html */
   template: `
+  <div>{{ year }}{{ value }}</div>
   <button v-on:click="updateGraph">更新</button>
   <div class="graph">
     <div
@@ -27,10 +30,14 @@ const PopulationBarPlot = {
     // 人口の配列 (State) を、RESAS API を用いて更新する関数
     async updateGraph() {
       if (this.prefecture !== null) {
-        const data = await getPopulations(this.api, this.prefecture);
+        const data = await getPopulations(this.api, this.prefecture, this.year, this.value);
 
         this.populations = data.map((obj) => obj.value);
         this.maxPopulation = Math.max(... data.map((obj) => obj.value));
+        ye = ye['result']['data']['data']['0']['year'];
+        this.year = ye;
+        va = va['result']['data']['data']['0']['value'];
+        this.value = va;
       } else {
         this.populations = [];
       }
